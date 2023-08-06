884301406604d499943cfcf1ddfa01a843a142fe
erdenay <erdenay@Ubuntu>
2023-08-06 16:12:02 +0200

    files updated for new data cells

--- apps/data-generator/insert_random_data.py
+++ apps/data-generator/insert_random_data.py
@@ -16,8 +16,11 @@ cursor = connection.cursor()
 for _ in range(100):
     name = fake.first_name()
     surname = fake.last_name()
-    order_id = random.randint(1000, 9999)  # Example of generating a random order ID
-    cursor.execute("INSERT INTO customer_data (customerName, customerSurname, customerOrderId) VALUES (%s, %s, %s)", (name, surname, order_id))
+    order_id = random.randint(1000, 9999)
+    email = fake.email()
+    order_date = fake.date_time_this_year()
+    product_name = fake.catch_phrase()
+    cursor.execute("INSERT INTO customer_data (customerName, customerSurname, customerOrderId, customerEmail, orderDate, productName) VALUES (%s, %s, %s, %s, %s, %s)", (name, surname, order_id, email, order_date, product_name))
 
 connection.commit()
 cursor.close()
